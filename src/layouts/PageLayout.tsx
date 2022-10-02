0c3e1710f2ab0d659aa3a5c9c6456946a9523581
Davide Ciulla <davide.ciulla@hotmail.com>
2022-10-03 00:54:38 +0200

    Added key to wrapping element

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -13,22 +13,22 @@ interface IPageLayout {
   body: React.ReactNode;
 }
 
-const Divider = () => <div className="h-3 w-px bg-brand-blue-200" />;
+const Divider = ({ className = "" }: { className?: string }) => (
+  <div className={`${className} h-3 w-px bg-brand-blue-200`.trim()} />
+);
 
 const Menu = ({ buttonGroups }: { buttonGroups: IPill[][] }) => {
   return (
     <div className="flex items-center gap-5">
       {buttonGroups.map((buttonGroup, idx) => (
-        <>
-          <div key={uuidv4()} className="flex gap-[10px]">
-            {buttonGroup
-              .filter((button) => !button.hidden)
-              .map((button) => (
-                <Pill key={uuidv4()} {...button} />
-              ))}
-          </div>
-          {idx < buttonGroups.length - 1 && <Divider />}
-        </>
+        <div key={uuidv4()} className="flex items-center gap-[10px]">
+          {buttonGroup
+            .filter((button) => !button.hidden)
+            .map((button) => (
+              <Pill key={uuidv4()} {...button} />
+            ))}
+          {idx < buttonGroups.length - 1 && <Divider className="ml-[10px]" />}
+        </div>
       ))}
     </div>
   );
@@ -65,7 +65,7 @@ export const PageLayout = ({
   return (
     <AppLayout>
       {side}
-      <div className="flex flex-auto flex-col items-center gap-10 bg-brand-blue-400 px-4 pt-12 pb-32 text-brand-white-100 md:overflow-scroll md:px-14">
+      <div className="flex flex-auto flex-col items-center gap-10 bg-brand-blue-400 px-4 pt-12 pb-32 text-brand-white-100 md:overflow-scroll lg:px-14">
         <Image src="/f1_logo.svg" alt="" width={120} height={30} />
         <PageContext title={title} subtitle={subtitle} />
         {buttons && <Menu buttonGroups={buttons} />}
