105b975174d10c4df725c8cfceddbf685d402547
Davide Ciulla <davide.ciulla@hotmail.com>
2022-10-18 09:54:40 +0200

    Added scroll snap to cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -6,9 +6,13 @@ interface ICards<T> {
 
 export const Cards = <T,>({ data, renderCard, keyExtractor }: ICards<T>) => {
   return (
-    <aside className="flex shrink-0 gap-3 overflow-scroll border-t-[1px] border-brand-blue-200 bg-brand-blue-300 p-5 md:flex-col md:border-r-[1px] z-[99] shadow-cards">
+    <aside className="flex snap-x snap-mandatory shrink-0 gap-3 overflow-scroll border-t-[1px] border-brand-blue-200 bg-brand-blue-300 p-5 md:flex-col md:border-r-[1px] z-[99] shadow-cards">
       {data.map((card) => {
-        return <div key={keyExtractor(card)}>{renderCard(card)}</div>;
+        return (
+          <div key={keyExtractor(card)} className="snap-center snap-always">
+            {renderCard(card)}
+          </div>
+        );
       })}
     </aside>
   );
