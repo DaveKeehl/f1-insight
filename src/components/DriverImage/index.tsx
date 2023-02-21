26d79216305c237bc89f76ef9bd4be164ba0d606
Davide Ciulla <davide.ciulla@hotmail.com>
2023-02-21 19:11:19 +0100

    Fixing driver images

--- src/components/DriverImage/index.tsx
+++ src/components/DriverImage/index.tsx
@@ -14,7 +14,7 @@ export const DriverImage = ({ givenName, familyName }: IDriverImage) => {
     <>
       <div className="absolute bottom-0 left-0 h-2/3 w-full bg-gradient-to-b from-transparent to-brand-blue-400" />
       <Image
-        src={`/drivers/front/${givenName}-${clean.familyName}.jpeg`}
+        src={`/drivers/front/${givenName.toLowerCase()}-${clean.familyName.toLowerCase()}.jpeg`}
         alt={`${givenName} ${familyName}`}
         width={1920}
         height={1920}
