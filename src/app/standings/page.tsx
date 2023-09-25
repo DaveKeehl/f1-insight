1039459a56bab8804e3c4ee5d016c73014cd6e30
Davide Ciulla <davide.ciulla@hotmail.com>
2023-09-25 23:54:03 +0200

    Removed console.log

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -16,8 +16,6 @@ export default async function StandingsPage() {
   const driverStandings = await getDriverStandings();
   const constructorStandings = await getConstructorStandings();
 
-  console.log({ driverStandings });
-
   return (
     <div className="relative flex w-full flex-col items-center gap-10 overflow-y-auto px-4 pb-8 pt-12 md:pb-16 lg:px-14">
       <Image src="/f1-logo.svg" alt="Formula 1 logo" width={120} height={30} />
